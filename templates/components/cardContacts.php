48de0885c97c542935b7d0dcb699215d06278b8c
bastiencouder <bastien.couder@gmail.com>
2023-10-10 15:18:38 +0200

    annuaire

--- templates/components/cardContacts.php
+++ templates/components/cardContacts.php
@@ -22,11 +22,29 @@ function getSpecialiteClass($specialite) {
     }
 }
 
+function getYearClass($year) {
+    switch ($year) {
+        case 'A1':
+            return 'px-3 py-2 border-2 border-green-700';
+        case 'A2':
+            return 'px-3 py-2 border-2 border-green-800';
+        case 'A3':
+            return 'px-3 py-2 border-2 border-green-900';
+        case 'M1':
+            return 'px-3 py-2 border-2 border-orange-800';
+        case 'M2':
+            return 'px-3 py-2 border-2 border-orange-900';
+        default:
+            return 'px-3 py-2 border-2 border-black';
+    }
+}
+
 if (!empty($selectedFiltered)) {
     if (!empty($filtered)) {
         echo "<ul class='py-8 flex flex-col gap-8 mr-4 mb-8'>";
         foreach ($filtered as $item) {
             $specialiteClass = getSpecialiteClass($item['specialite']);
+            $yearClass = getYearClass($item['year']);
             ?>
             <li class='px-3 w-full flex flex-col space-y-4 p-4 border-2 border-black lg:w-[40rem]'>
                 <div class='flex gap-4 font-bold capitalize items-center text-2xl'>
@@ -35,9 +53,9 @@ if (!empty($selectedFiltered)) {
                 </div>
                 <div class='flex items-center gap-4'>
                     <p class='<?= $specialiteClass ?>'><?= $item['specialite'] ?></p>
-                    <p class='px-3 py-2 border-2 border-green-700'><?= $item['year'] ?></p>
+                    <p class='<?= $yearClass ?>'><?= $item['year'] ?></p>
                 </div>
-                <div class='flex lg:flex-col flex-row gap-12'>
+                <div class='flex flex-col lg:flex-row gap-x-12 gap-y-2'>
                     <p class=''><i class='fa-solid fa-envelope mr-4'></i><?= $item['email'] ?></p>
                     <p class=''><i class='fa-solid fa-phone mr-4'></i><?= $item['tel'] ?></p>
                 </div>
@@ -52,6 +70,7 @@ if (!empty($selectedFiltered)) {
     echo "<ul class='py-8 flex flex-col gap-8 mr-4 mb-8'>";
     foreach ($data as $item) {
         $specialiteClass = getSpecialiteClass($item['specialite']);
+        $yearClass = getYearClass($item['year']);
         ?>
         <li class='ml-2 w-full flex flex-col space-y-4 p-4 border-2 border-black lg:w-[40rem]'>
             <div class='flex gap-4 font-bold capitalize items-center text-2xl'>
@@ -60,7 +79,7 @@ if (!empty($selectedFiltered)) {
             </div>
             <div class='flex items-center gap-4'>
                 <p class='<?= $specialiteClass ?>'><?= $item['specialite'] ?></p>
-                <p class='px-3 py-2 border-2 border-green-700'><?= $item['year'] ?></p>
+                <p class='<?= $yearClass ?>'><?= $item['year'] ?></p>
             </div>
             <div class='flex flex-col lg:flex-row gap-x-12 gap-y-2'>
                 <p class=''><i class='fa-solid fa-envelope mr-4'></i><?= $item['email'] ?></p>
