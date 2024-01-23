fb85a05ff6b6bff9effdd7b0d7fd38b265326b1a
bastiencouder <bastien.couder@gmail.com>
2024-01-23 23:19:53 +0100

    annuaire

--- configs/global.php
+++ configs/global.php
@@ -1,12 +1,14 @@
 <?php 
+$dbConfig = json_decode(file_get_contents('./config.json'), true);
+
 $globalConfigs = 
 [
     "debugMode" => true,
     "database" => [
-        "db_name" => "annuaire",
-        "host" => "localhost",
-        "port" => 3306,
-        "user" => "bastiencdr",
-        "password" => "bstcd"
+        "db_name" => $dbConfig['db_name'],
+        "host" => $dbConfig['host'],
+        "port" => $dbConfig['port'],
+        "user" => $dbConfig['user'],
+        "password" => $dbConfig['password']
     ]
 ];
--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
-
 require_once './configs/bootstrap.php';
+require_once './templates/layouts/html.layout.php';
 ob_start();
 
-if(isset($_GET["page"])){
-    fromInc($_GET['page']);
+if (isset($_GET["page"])) {
+    fromInc($_GET["page"]);
+} else {
+    fromInc("accueil");
 }
 
-$pageContent = [
-    "html" => ob_get_clean(),
-
-];
-
-include "./templates/layouts/". $_GET["layout"] .".layout.php";
-
+?>
